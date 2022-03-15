407ef5d1edc7e43730254d02013bb2b354eade00
Kclient <go4521304@gmail.com>
2022-03-15 22:15:28 +0900

    초기 커밋.

---
+++ Direct3D Win32 Game1/pch.h
@@ -0,0 +1,77 @@
+//
+// pch.h
+// Header for standard system include files.
+//
+
+#pragma once
+
+#include <winsdkver.h>
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0A00
+#endif
+#include <sdkddkver.h>
+
+// Use the C++ standard templated min/max
+#define NOMINMAX
+
+// DirectX apps don't need GDI
+#define NODRAWTEXT
+#define NOGDI
+#define NOBITMAP
+
+// Include <mcx.h> if you need this
+#define NOMCX
+
+// Include <winsvc.h> if you need this
+#define NOSERVICE
+
+// WinHelp is deprecated
+#define NOHELP
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+#include <wrl/client.h>
+#include <wrl/event.h>
+
+#ifdef USING_DIRECTX_HEADERS
+#include <directx/dxgiformat.h>
+#include <directx/d3d12.h>
+#else
+#include <d3d12.h>
+#endif
+
+#include <dxgi1_4.h>
+#include <DirectXMath.h>
+#include <DirectXColors.h>
+
+#include "d3dx12.h"
+
+#include <algorithm>
+#include <cmath>
+#include <cstddef>
+#include <cstdint>
+#include <cstring>
+#include <cwchar>
+#include <exception>
+#include <iterator>
+#include <memory>
+#include <stdexcept>
+#include <system_error>
+#include <tchar.h>
+
+#ifdef _DEBUG
+#include <dxgidebug.h>
+#endif
+
+namespace DX
+{
+    inline void ThrowIfFailed(HRESULT hr)
+    {
+        if (FAILED(hr))
+        {
+            // Set a breakpoint on this line to catch DirectX API errors
+            throw std::exception();
+        }
+    }
+}
