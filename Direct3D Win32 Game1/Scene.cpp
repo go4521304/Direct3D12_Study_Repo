047bd1196de9ff708ae3a2fd763a83ce69fd3eb6
Kclient <go4521304@gmail.com>
2022-03-17 22:01:01 +0900

    Scene 생성중 - rootsignature

--- Direct3D Win32 Game1/Scene.cpp
+++ Direct3D Win32 Game1/Scene.cpp
@@ -1,2 +1,43 @@
-#include "pch.h"
 #include "Scene.h"
+
+Scene::Scene()
+{
+}
+
+Scene::~Scene()
+{
+}
+
+bool Scene::OnProcessingMouseMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM lParam)
+{
+	return false;
+}
+
+bool Scene::OnProcessingKeyboardMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM lParam)
+{
+	return false;
+}
+
+void Scene::CreateGraphicsRootSignature(ID3D12Device* pd3dDevice)
+{
+	D3D12_ROOT_SIGNATURE_DESC d3dRootSignatureDesc;
+
+	::ZeroMemory(&d3dRootSignatureDesc, sizeof(D3D12_ROOT_SIGNATURE_DESC));
+
+	d3dRootSignatureDesc.NumParameters = 0;
+	d3dRootSignatureDesc.pParameters = NULL;
+	d3dRootSignatureDesc.NumStaticSamplers = 0;
+	d3dRootSignatureDesc.pStaticSamplers = NULL;
+	d3dRootSignatureDesc.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
+	ID3DBlob* pd3dSignatureBlob = NULL;
+	ID3DBlob* pd3dErrorBlob = NULL;
+
+	::D3D12SerializeRootSignature(&d3dRootSignatureDesc, D3D_ROOT_SIGNATURE_VERSION_1, &pd3dSignatureBlob, &pd3dErrorBlob);
+	pd3dDevice->CreateRootSignature(0, pd3dSignatureBlob->GetBufferPointer(),
+		pd3dSignatureBlob->GetBufferSize(), __uuidof(ID3D12RootSignature), (void
+			**)&m_pd3dGraphicsRootSignature);
+	if (pd3dSignatureBlob) pd3dSignatureBlob->Release();
+	if (pd3dErrorBlob) pd3dErrorBlob->Release();
+}
+
+}
--- Direct3D Win32 Game1/Scene.h
+++ Direct3D Win32 Game1/Scene.h
@@ -3,6 +3,26 @@
 
 class Scene
 {
+public:
+	Scene();
+	~Scene();
+
+	bool OnProcessingMouseMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM lParam);
+	bool OnProcessingKeyboardMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM lParam);
+
+	void CreateGraphicsRootSignature(ID3D12Device* pd3dDevice);
+	void CreateGraphicsPipelineState(ID3D12Device* pd3dDevice);
+
+	void BuildObjects(ID3D12Device* pd3dDevice);
+	void ReleaseObjects();
+
+	bool ProcessInput();
+	void AnimateObjects(float fTimeElapsed);
+
+	void PrepareRender(ID3D12GraphicsCommandList* pd3dCommandList);
+	void Render(ID3D12GraphicsCommandList* pd3dCommandList);
+
 private:
-	
+	ID3D12RootSignature*							m_pd3dGraphicsRootSignature;
+	Microsoft::WRL::ComPtr<ID3D12PipelineState>		m_pd3dPipelineState;
 };
